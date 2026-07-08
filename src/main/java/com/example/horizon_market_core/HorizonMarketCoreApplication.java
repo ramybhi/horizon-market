8c710345ba9f496ea6f9f8c2d5f43b7cf29d764f
ramybhi <ramyapenta96@gmail.com>
2026-07-08 13:18:02 +0530

    Initial commit: Horizon Market Core - Bootstrap

---
+++ src/main/java/com/example/horizon_market_core/HorizonMarketCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.horizon_market_core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HorizonMarketCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HorizonMarketCoreApplication.class, args);
+	}
+
+}
